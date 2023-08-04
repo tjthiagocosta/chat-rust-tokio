3031f68a954ad5ea6113be64298253a384114073
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-08-04 10:57:32 -0300

    add multi client chat

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (tx, _rx) = broadcast::channel(10);
 
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
         tokio::spawn(async move {
             let (read, mut writer) = socket.split();
             let mut reader = BufReader::new(read);
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
                     break;
                 }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
 
-                writer.write_all(&line.as_bytes()).await.unwrap();
-                line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            writer.write_all(&msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
             }
         });
     }
