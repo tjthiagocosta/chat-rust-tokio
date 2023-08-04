5e6eceb442130a144f90f8d87c68baeb6960ae60
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-08-03 22:37:18 -0300

    implemented echo chat for multiple clients

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let (read, mut writer) = socket.split();
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+
+                writer.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
+    }
+}
